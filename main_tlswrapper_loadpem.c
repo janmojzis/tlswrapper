2c09bd04d8561b3071f223750ec7e198bde30959
Jan Mojžíš <jan.mojzis@gmail.com>
2021-11-09 07:26:23 +0100

    tlswrapper-loadpem update

--- main_tlswrapper_loadpem.c
+++ main_tlswrapper_loadpem.c
@@ -1,40 +1,128 @@
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 #include "randombytes.h"
 #include "log.h"
+#include "alloc.h"
 #include "tls.h"
 #include "writeall.h"
 
-struct tls_pem ctx = {0};
-unsigned char key[32];
+static struct tls_pem pem = {0};
+static struct tls_pubcrt crt = {0};
+static struct tls_seccrt keycrt = {0};
+static unsigned char key[32];
 
-void usage(void) {
-    log_u1("tlswrapper-loadpem usage: tlswrapper-loadpem filename");
-    _exit(100);
+static int flagverbose = 1;
+static int flagpublic = 0;
+static int flagsecret = 0;
+
+static char *fnin = 0;
+static char *fnout = 0;
+static int fdout = 1;
+
+static void cleanup(void) {
+    tls_pem_free(&pem);
+    randombytes(&crt, sizeof crt);
+    randombytes(&keycrt, sizeof keycrt);
+    randombytes(key, sizeof key);
+    alloc_freeall();
+    {
+        unsigned char stack[4096];
+        randombytes(stack, sizeof stack);
+    }
+    (void) fsync(fdout);
+    (void) close(fdout);
 }
 
+static int die(int x) {
+    cleanup();
+    _exit(x);
+}
+
+static void usage(void) {
+    log_u1("tlswrapper-loadpem usage: tlswrapper-loadpem [ -qQvpPsS ] [ -o filename ] -i filename");
+    die(100);
+}
 
 int main_tlswrapper_loadpem(int argc, char **argv) {
 
+    char *x;
+
     log_name("tlswrapper-loadpem");
-    log_level(1);
 
     (void) argc;
     if (!argv[0]) usage();
-    if (!argv[1]) usage();
+    for (;;) {
+        if (!argv[1]) break;
+        if (argv[1][0] != '-') break;
+        x = *++argv;
+        if (x[0] == '-' && x[1] == 0) break;
+        if (x[0] == '-' && x[1] == '-' && x[2] == 0) break;
+        while (*++x) {
+            if (*x == 'q') { flagverbose = 0; log_level(flagverbose); continue; }
+            if (*x == 'Q') { flagverbose = 1; log_level(flagverbose); continue; }
+            if (*x == 'v') { log_level(++flagverbose); continue; }
+            if (*x == 'p') { flagpublic = 1; continue; }
+            if (*x == 'P') { flagpublic = 0; continue; }
+            if (*x == 's') { flagsecret = 1; continue; }
+            if (*x == 'S') { flagsecret = 0; continue; }
+            if (*x == 'i') {
+                if (x[1]) { fnin = x + 1; break; }
+                if (argv[1]) { fnin = *++argv; break; }
+            }
+            if (*x == 'o') {
+                if (x[1]) { fnout = x + 1; break; }
+                if (argv[1]) { fnout = *++argv; break; }
+            }
+            usage();
+        }
+    }
+    log_time(1);
 
-    log_level(4);
+    if (!fnin) usage();
+    if (!fnout) fnout = "-";
+    if (strcmp(fnout, "-")) {
+        fdout = open(fnout, O_CREAT | O_WRONLY | O_NONBLOCK, 0644);
+        if (fdout == -1) {
+            log_f3("unable to open file ", fnout, " for writing");
+            die(111);
+        }
+    }
 
     randombytes(key, sizeof key);
 
-    if (!tls_pem_load(&ctx, argv[1], key)) {
-        log_f2("unable to load pem file ", argv[1]);
-        _exit(111);
-    }
-    /* tls_pem_decrypt(&ctx, key); */
-    if (writeall(1, ctx.pub, ctx.publen) == -1) {
-        log_f1("unable to write output");
-        _exit(111);
+    if (!tls_pem_load(&pem, fnin, key)) {
+        log_f2("unable to load pem file ", fnin);
+        die(111);
     }
 
-    _exit(0);
+
+    if (flagpublic) {
+        if (!tls_pubcrt_parse(&crt, pem.pub, pem.publen)) {
+            log_f2("unable to parse PEM public-object from the file ", fnin);
+            die(111);
+        }
+        if (writeall(fdout, pem.pub, pem.publen) == -1) {
+            log_f2("unable to write output to the file ", fnout);
+            die(111);
+        }
+    }
+
+    if (flagsecret) {
+        tls_pem_decrypt(&pem, key);
+        if (!tls_seccrt_parse(&keycrt, pem.sec, pem.seclen)) {
+            log_f2("unable to parse PEM secret-object from the file ", fnin);
+            die(111);
+        }
+
+        if (writeall(fdout, pem.sec, pem.seclen) == -1) {
+            log_f2("unable to write output to the file ", fnout);
+            die(111);
+        }
+    }
+
+    die(0);
+    return 0; /* make compiler happy */
 }
