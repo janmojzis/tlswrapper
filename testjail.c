c3f15f6ad786fd5bb87475f7da409ee56b6b7d16
Jan Mojžíš <jan.mojzis@gmail.com>
2021-11-17 17:35:46 +0100

    testjail.c detect short group

--- testjail.c
+++ testjail.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <grp.h>
 #include "log.h"
 #include "jail.h"
 
@@ -17,6 +18,20 @@ static int run(void (*op)(void)) {
     while (waitpid(pid, &status, 0) != pid) {};
     if (!WIFEXITED(status)) return 222;
     return WEXITSTATUS(status);
+}
+
+void detectshortgroup(void) {
+    short x[4];
+
+    x[0] = x[1] = 1;
+    if (getgroups(1, (gid_t *) x) == 0) if (setgroups(1, (gid_t *) x) == -1) _exit(1);
+
+    if (getgroups(1, (gid_t *) x) == -1) _exit(1);
+    if (x[1] != 1) _exit(1);
+    x[1] = 2;
+    if (getgroups(1, (gid_t *) x) == -1) _exit(1);
+    if (x[1] != 2) _exit(1);
+    _exit(0);
 }
 
 void testjail1(void) {
@@ -42,8 +57,13 @@ int main() {
     log_name("testjail");
 
 
-    log_i1("testjail1");
-    if (run(testjail1) != 0) return 111;
+    if (run(detectshortgroup) == 0) {
+        log_w1("testjail1 skipped: shortgoup detected");
+    }
+    else {
+        log_i1("testjail1");
+        if (run(testjail1) != 0) return 111;
+    }
 
     log_i1("testjail2");
     if (run(testjail2) != 0) return 111;
