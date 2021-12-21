584326c71d32c0ab9e1360eee2e6dcf8b623a627
Jan Mojžíš <jan.mojzis@gmail.com>
2021-12-21 21:50:20 +0100

    test-proxyprotocol.sh not so verbose test

--- test-proxyprotocol.sh
+++ test-proxyprotocol.sh
@@ -54,5 +54,6 @@ ${CMD} -w tlswrappernojail -P2 -d "`pwd`/testcerts" sh -c 'exec cat >&9' <data.i
   echo "PROXY_TCP4_255.255.255.255_255.255.255.255_65536_65535"
   echo "PROXY_TCP4_255.255.255.255_255.255.255.255_65535_65536"
 ) | while read line; do
-  ${CMD} -P "${line}" -w tlswrappernojail -p1 -P1 -d "`pwd`/testcerts" -v sh -c 'exec cat >&9' <data.in 9>&1 2>&1
+  ${CMD} -P "${line}" -w tlswrappernojail -p1 -P1 -q -d "`pwd`/testcerts" sh -c 'exec cat >&9' <data.in 9>&1 2>&1
+  echo $?
 done
