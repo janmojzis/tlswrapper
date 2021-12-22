f82d91b6b80d408a576848d84044e9be510e1dc1
Jan Mojžíš <jan.mojzis@gmail.com>
2021-12-22 12:10:47 +0100

    test-options.sh fix sed

--- test-options.sh
+++ test-options.sh
@@ -19,92 +19,92 @@ ln -s testfile testfilelink
 
 # certs
 echo 'tlswrapper rejects fifo as a certdir'
-tlswrappernojail -q -d testfifo </dev/null 2>&1
+tlswrappernojail -Q -d testfifo </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects fifo as a certfile'
-tlswrappernojail -q -f testfifo </dev/null 2>&1
+tlswrappernojail -Q -f testfifo </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects fifo as a anchorfile'
-tlswrappernojail -q -a testfifo </dev/null 2>&1
+tlswrappernojail -Q -a testfifo </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects nonexistent file as a certdir'
-tlswrappernojail -q -d notexist </dev/null 2>&1
+tlswrappernojail -Q -d notexist </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects nonexistent file as a certfile'
-tlswrappernojail -q -f notexist </dev/null 2>&1
+tlswrappernojail -Q -f notexist </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects nonexistent file as a anchorfile'
-tlswrappernojail -q -a notexist </dev/null 2>&1
+tlswrappernojail -Q -a notexist </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects regular file as a certdir'
-tlswrappernojail -q -d testfile </dev/null 2>&1
+tlswrappernojail -Q -d testfile </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects directory as a certfile'
-tlswrappernojail -q -f testdir </dev/null 2>&1
+tlswrappernojail -Q -f testdir </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects directory as a anchor file'
-tlswrappernojail -q -a testdir </dev/null 2>&1
+tlswrappernojail -Q -a testdir </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects link to regular file as a certdir'
-tlswrappernojail -q -d testfilelink </dev/null 2>&1
+tlswrappernojail -Q -d testfilelink </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects link to directory as a certfile'
-tlswrappernojail -q -f testdirlink </dev/null 2>&1
+tlswrappernojail -Q -f testdirlink </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects link to directory as a anchorfile'
-tlswrappernojail -q -a testdirlink </dev/null 2>&1
+tlswrappernojail -Q -a testdirlink </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 
 # TLS version
 echo 'tlswrapper rejects bad min TLS version'
-tlswrappernojail -q -f testfile -m xxx </dev/null 2>&1
+tlswrappernojail -Q -f testfile -m xxx </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects bad max TLS version'
-tlswrappernojail -q -f testfile -M xxx </dev/null 2>&1
+tlswrappernojail -Q -f testfile -M xxx </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 
 # ciphers
 echo 'tlswrapper rejects bad ciphername'
-tlswrappernojail -q -f testfile -c xxx </dev/null 2>&1
+tlswrappernojail -Q -f testfile -c xxx </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 
 # ephemeral 
 echo 'tlswrapper rejects bad ephemeral name'
-tlswrappernojail -q -f testfile -e xxx </dev/null 2>&1
+tlswrappernojail -Q -f testfile -e xxx </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 
 # proxy protocol
 echo 'tlswrapper rejects bad proxy protocol version'
-tlswrappernojail -q -f testfile -P 9 </dev/null 2>&1
+tlswrappernojail -Q -f testfile -P 9 </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 
 # user from client cert. 
 echo 'tlswrapper rejects bad ASN.1 object'
-tlswrappernojail -q -f testfile -U xxx </dev/null 2>&1
+tlswrappernojail -Q -f testfile -U xxx </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 
 # timeouts
 echo 'tlswrapper rejects zero number as a network timeout'
-tlswrappernojail -q -f testfile -t0 true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -t0 true </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects zero number as a handshake timeout'
-tlswrappernojail -q -f testfile -T0 true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -T0 true </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects negative number as a network timeout'
-tlswrappernojail -q -f testfile -t -1 true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -t -1 true </dev/null 2>&1
 echo $?; echo
 echo 'tlswrapper rejects nefative number as a handshake timeout'
-tlswrappernojail -q -f testfile -T -1 true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -T -1 true </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects bad number as a network timeout'
-tlswrappernojail -q -f testfile -t badtimeout true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -t badtimeout true </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects bad numbern as a handshake timeout'
-tlswrappernojail -q -f testfile -T badtimeout true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -T badtimeout true </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects too large number as a network timeout'
-tlswrappernojail -q -f testfile -t 86401 true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -t 86401 true </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
 echo 'tlswrapper rejects too large numbern as a handshake timeout'
-tlswrappernojail -q -f testfile -T 86401 true </dev/null 2>&1
+tlswrappernojail -Q -f testfile -T 86401 true </dev/null 2>&1 | sed 's/ (.*)/ /'
 echo $?; echo
