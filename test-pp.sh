88c89fc226b4e5ec2e87cbf6a778d247ad716c93
Jan Mojžíš <jan.mojzis@gmail.com>
2021-12-22 20:57:13 +0100

    test-pp remove od, use escape

--- test-pp.sh
+++ test-pp.sh
@@ -23,20 +23,20 @@ CMD="tlswrapper-test -q"
 CMD="${CMD} -d `cat testcerts/days`"
 CMD="${CMD} -a testcerts/ca-ec-prime256v1.pem"
 CMD="${CMD} -h okcert-ec-prime256v1-ec-prime256v1-ok.pem"
-${CMD} -w tlswrappernojail -P2 -d "testcerts" sh -c 'exec cat >&9' <data.in 9>&1 2>&1 | od -c
-${CMD} -w tlswrappernojail -P1 -d "testcerts" sh -c 'exec >&9; printenv TCPREMOTEIP; cat' <data.in 9>&1 2>&1
+${CMD} -w tlswrappernojail -P2 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
+${CMD} -w tlswrappernojail -P1 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
 TCPREMOTEIP=1.2.3.4; export TCPREMOTEIP
 TCPREMOTEPORT=1234; export TCPREMOTEPORT
 TCPLOCALIP=1.2.3.4; export TCPLOCALIP
 TCPLOCALPORT=1234; export TCPLOCALPORT
-${CMD} -w tlswrappernojail -P2 -d "testcerts" sh -c 'exec cat >&9' <data.in 9>&1 2>&1 | od -c
-${CMD} -w tlswrappernojail -P1 -d "testcerts" sh -c 'exec >&9; printenv TCPREMOTEIP; cat' <data.in 9>&1 2>&1
+${CMD} -w tlswrappernojail -P2 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
+${CMD} -w tlswrappernojail -P1 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
 TCPREMOTEIP="abcd:abcd:abcd:abcd:abcd:abcd:abcd:abcd"; export TCPREMOTEIP
 TCPREMOTEPORT=1234; export TCPREMOTEPORT
 TCPLOCALIP=abcd:abcd:abcd:abcd:abcd:abcd:abcd:abcd; export TCPLOCALIP
 TCPLOCALPORT=1234; export TCPLOCALPORT
-${CMD} -w tlswrappernojail -P2 -d "testcerts" sh -c 'exec cat >&9' <data.in 9>&1 2>&1 | od -c
-${CMD} -w tlswrappernojail -P1 -d "testcerts" sh -c 'exec >&9; printenv TCPREMOTEIP; cat' <data.in 9>&1 2>&1
+${CMD} -w tlswrappernojail -P2 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
+${CMD} -w tlswrappernojail -P1 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
 
 (
   echo "PROXY_UNKNOWN"
@@ -44,8 +44,8 @@ ${CMD} -w tlswrappernojail -P1 -d "testcerts" sh -c 'exec >&9; printenv TCPREMOT
   echo "PROXY_TCP6_ffff:ffff:ffff:ffff:ffff:ffff:255.255.255.255_ffff:ffff:ffff:ffff:ffff:ffff:255.255.255.255_65535_65535"
   echo "PROXY_TCP4_255.255.255.255_255.255.255.255_65535_65535"
 ) | while read line; do
-  ${CMD} -P "${line}" -w tlswrappernojail -p1 -P2 -d "testcerts" sh -c 'exec cat >&9' <data.in 9>&1 2>&1 | od -c
-  ${CMD} -P "${line}" -w tlswrappernojail -p1 -P1 -d "testcerts" sh -c 'exec >&9; printenv TCPREMOTEIP; cat' <data.in 9>&1 2>&1
+  ${CMD} -P "${line}" -w tlswrappernojail -p1 -P2 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
+  ${CMD} -P "${line}" -w tlswrappernojail -p1 -P1 -d "testcerts" sh -c 'exec ./escape >&9' <data.in 9>&1 2>&1
 done
 
 (
