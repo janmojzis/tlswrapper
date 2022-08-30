0cb7bb4507dc877a6f3e79a5e34d266340923367
Jan Mojžíš <jan.mojzis@gmail.com>
2022-08-30 15:33:51 +0200

    randombytes.c fallback to /dev/urandom when getentropy() doesn't exist (#8)

--- randombytes.c
+++ randombytes.c
@@ -1,30 +1,9 @@
-/*
-version 20220222
-*/
-
-#include <unistd.h>
-#ifdef __APPLE__
-#include <sys/random.h>
-#endif
 #include "randombytes.h"
 
-void randombytes(void *xv, long long xlen) {
+#ifdef randombytes_getentropy
+#include "randombytes.c-01getentropy"
+#endif
 
-    long long i;
-    unsigned char *x = (unsigned char *) xv;
-
-    while (xlen > 0) {
-        if (xlen < 256)
-            i = xlen;
-        else
-            i = 256;
-
-        if (getentropy(x, i) == -1) {
-            sleep(1);
-            continue;
-        }
-        x += i;
-        xlen -= i;
-    }
-    __asm__ __volatile__("" : : "r"(xv) : "memory");
-}
+#ifdef randombytes_devurandom
+#include "randombytes.c-02devurandom"
+#endif
