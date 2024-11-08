355f35151f5599b2bc21df32d75ddf5ebd0fd35b
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-08 07:22:05 +0100

    tls_ecdsa.c: fix mixed declarations and code

--- tls_ecdsa.c
+++ tls_ecdsa.c
@@ -3,8 +3,8 @@
 uint32_t tls_ecdsa_vrfy_asn1(const br_ec_impl *impl, const void *hash,
                              size_t hash_len, const br_ec_public_key *pk,
                              const void *sig, size_t sig_len) {
-    (void) impl;
     br_ecdsa_vrfy vrfy = br_ecdsa_vrfy_asn1_get_default();
+    (void) impl;
 
     return vrfy(br_ec_get_default(), hash, hash_len, pk, sig, sig_len);
 }
